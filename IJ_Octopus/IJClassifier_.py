79e315200cafca65bae16786fe94d83650b54413
quantumjot <code@arlowe.co.uk>
2016-07-20 08:53:14 +0100

    Added new ImageJ plugin to deal with generating classifier training data from an image stream

---
+++ IJ_Octopus/IJClassifier_.py
@@ -0,0 +1,299 @@
+"""
+ImageJ plugin to export training data for generation of classifiers
+
+Takes one to two image data streams, and crops out ROIs around 
+centroids defined by the ImageJ multipoint ROI. Packages these up
+for use by the machine learning classifiers.
+
+Functions:
+
+Notes:
+
+Changes:
+
+Lowe, A.R. 2016
+code@arlowe.co.uk
+"""
+
+__version__ = "0.01a"
+__author__ = "Alan R. Lowe"
+__email__ = "code@arlowe.co.uk"
+
+
+from ij.io import FileInfo, OpenDialog, FileOpener, ImageWriter, DirectoryChooser
+from ij.gui import GenericDialog, Roi, PointRoi, TrimmedButton
+from ij.measure import ResultsTable
+from ij import ImagePlus, ImageStack, WindowManager
+import ij.IJ as IJ
+from ij.plugin.frame import RoiManager
+from java.awt.event import ActionListener
+
+import re
+import os
+import pickle
+
+
+DEFAULT_OUTPUT_PATH = "/Users/ubcg83a/Desktop/test/"
+
+
+class IJClassifier_(object):
+	def __init__(self):
+		self.path = DEFAULT_OUTPUT_PATH
+		self.window_size = 30
+
+	def __call__(self):
+
+		# open a dialog menu and get the options
+		opts = self.dialog()
+
+		# now grab the ROIs and write out the classifier
+		ROIs = self.grab(opts)
+		self.write(ROIs, opts)
+
+
+	def dialog(self):
+		"""
+		Open the classifier dialog window and return the paramters
+		chosen.
+		"""
+	
+		# determine how many images are currently open
+		image_count = WindowManager.getImageCount()
+		image_titles = list(WindowManager.getImageTitles())
+		image_titles.append("None")
+	
+		# now open a dialog to let the user set options
+
+		path_listener = SetPathListener(self.path)
+		path_button = TrimmedButton("Output directory",0)
+		path_button.addActionListener(path_listener)
+		
+		dlg = GenericDialog("Create classifier data (v"+__version__+")")
+		dlg.addMessage("Select the ROI you want to save")
+		dlg.addNumericField("Window size (pixels): ", self.window_size, 0);
+		dlg.addChoice("Image file:",image_titles,"None")
+		dlg.addChoice("Mask file:",image_titles,"None")
+		dlg.addCheckbox("Exclude edges", True)
+		dlg.addCheckbox("Zip classifier", True)
+		dlg.addCheckbox("Save classifier details", True)
+		dlg.add(path_button)
+		dlg.showDialog()
+	
+		# handle the cancelled dialog box
+		if dlg.wasCanceled():
+			return None
+	
+	
+		dialog_options = {'window_size': dlg.getNextNumber(), 'data': dlg.getNextChoice(),
+						  'mask': dlg.getNextChoice(), 'edges': dlg.getNextBoolean(),
+						  'zip': dlg.getNextBoolean(), 'save': dlg.getNextBoolean(),
+						  'path': path_listener.path}
+	
+		# check that we actually selected an image file
+		if dialog_options['data'] == "None":
+			w_dlg = GenericDialog("Warning")
+			w_dlg.addMessage("You must select an image stream.")
+			w_dlg.showDialog()
+			return None
+			
+		# grab the contents and return these as a dictionary
+		return dialog_options
+	
+	def grab(self, dialog_options=None):
+		""" Get the image patches using the ROI manager.
+		"""
+	
+		if not dialog_options:
+			return
+	
+		# get a refence to the ROI manager and check that we have ROIs!
+		rm = RoiManager.getRoiManager()
+		num_ROIs = rm.getCount()
+		if num_ROIs < 1: return None
+	
+		return [ClassifierROI(rm.getRoi(r), path=dialog_options['path']) for r in xrange(num_ROIs)]
+	
+
+
+	def write(self, ROIs=None, dialog_options=None):
+		""" Write out the classifier info 
+		"""
+
+		if not dialog_options or not isinstance(ROIs, list):
+			return
+
+		# get the data stack
+		data_stack = WindowManager.getImage( dialog_options['data'] )
+
+		# get the mask stack
+		if not dialog_options['mask'] == None:
+			mask_stack = WindowManager.getImage( dialog_options['mask'])
+		else:
+			mask_stack = None
+
+		for r in ROIs:
+			IJ.log("Grabbing patches for ROI: "+ r.name)
+			r.window_size = dialog_options['window_size']
+			r.save = dialog_options['save']
+			r(data=data_stack, mask=mask_stack)
+
+		# old style dictionary comprehension
+		ROI_dict = dict((r.name,{'frame':r.index, 'x':r.x, 'y':r.y}) for r in ROIs)
+		ROI_dict['opts'] = dialog_options
+
+		# save out a pickled dictionary with all of the details
+		if dialog_options['save']:
+			try:
+				classifier_file = open(os.path.join(dialog_options['path'],dialog_options['data']+'_classifier.p'), 'wb')
+				pickle.dump( ROI_dict, classifier_file, -1)
+				classifier_file.close()
+			except IOError:
+				IJ.log('Could not pickle classifier info file.')
+
+		print ROI_dict
+		
+
+
+
+
+class SetPathListener(ActionListener):
+	""" ActionListener subclass to deal with button presses in the
+	GUI. Returns the selected path from the DirectoryChooser
+	function.
+	"""
+	def __init__(self, path=DEFAULT_OUTPUT_PATH):
+		self.path = path
+
+	def actionPerformed(self, event):
+		dlg = DirectoryChooser("Choose an output directory for the classifier")
+
+		if os.path.exists(self.path):
+			dlg.setDefaultDirectory(self.path)
+		
+		self.path = dlg.getDirectory()
+		IJ.log("Added path: "+self.path)
+		
+		
+class ClassifierROI(object):
+	"""
+	ClassifierROI
+
+	A wrapper class to deal with multipoint ROIs and extracting
+	windows from image streams for use with the classifier.
+
+	Notes:
+		None
+	"""
+	def __init__(self, ROI=None, path=None):
+		if isinstance(ROI, PointRoi):
+			self.__ROI = ROI
+		else:
+			raise TypeError("ClassifierROI: Roi needs to be of type ImageJ PointROI")
+
+		self.save = False
+		self.window_size = 20
+		self.bounds = self.__ROI.getBounds()
+		
+		# is this a well formed path
+		if not isinstance(path, basestring):
+			raise TypeError("ClassifierROI: path must be specified as a string")
+
+		# check that the path exists
+		if not os.path.exists(path):
+			raise IOError("ClassifierROI: path does not exist ({0:s})".format(path))
+
+		self.__classifier_path = path
+
+	@property 
+	def name(self):
+		return self.__ROI.getName()
+
+	@property 
+	def index(self):
+		return self.__ROI.getPosition()
+
+	@property 
+	def datapath(self):
+		return self.__classifier_path
+
+	@property 
+	def x(self):
+		return list(self.__ROI.getPolygon().xpoints)
+	@property 
+	def y(self):
+		return list(self.__ROI.getPolygon().ypoints)
+
+	def __len__(self):
+		return self.__ROI.getCount( self.__ROI.getCounter() )
+
+	def __getitem__(self, pointID=None):	
+		if not isinstance(pointID, int):
+			raise TypeError("ClassifierROI: point ID must be an integer")
+
+		if pointID < 0 or pointID > len(self)-1:
+			raise Exception("ClassifierROI: point ID must be in the range 0 to {0:d}".format(len(self)-1))
+	
+		return ( self.x[pointID], self.y[pointID] )
+
+
+	def __call__(self, data=None, mask=None):
+		""" Loop through all of the points, extract a window from the image stack
+		and (optionally) this mask data. Crop a region and save the image out
+		to the specified path.
+		"""
+
+		if not isinstance(data, ImagePlus):
+			raise TypeError("ClassiferROI: image stack data must be of type ImageStack")
+
+
+		for i in xrange(len(self)):
+
+			x,y = self[i]
+			temp_ROI = Roi(x-self.window_size, y-self.window_size, self.window_size*2, self.window_size*2)
+
+			new_im = self.grab(data, temp_ROI)
+			new_im.setTitle(self.__ROI.name+"_"+str(i))
+			self.write(new_im)
+
+			if self.save:
+				new_im.close()
+			else:
+				new_im.show()
+
+
+			if not isinstance(mask, ImagePlus):
+				continue
+
+			new_im = self.grab(mask, temp_ROI)
+			new_im.setTitle("mask_"+self.__ROI.name+"_"+str(i))
+			self.write(new_im)
+
+			if self.save:
+				new_im.close()
+			else:
+				new_im.show()
+		
+		return
+
+	def grab(self, data=None, temp_ROI=None):
+		""" Grab the window around the ROI and return a new image
+		"""
+		
+		# go to the frame number, copy the region of the image, rename it then save it
+		data.setPosition(self.index)
+		data.setRoi(temp_ROI, False)
+		data.copy()
+
+		# now create a new image for this cropped region
+		return data.getClipboard()
+
+	def write(self, patch=None):
+		""" Write the patch to a file.
+		"""
+		if self.save:
+			IJ.save(patch, os.path.join(self.datapath, patch.getTitle()+".tif"))
+
+
+
+c = IJClassifier_()
+c()
